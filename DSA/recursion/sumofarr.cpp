e4f3abeae702a131402ab9725472c21dfaabd5f6
jitesh <jitesh.kumar05official@gmail.com>
2025-05-12 13:10:16 +0530

    recursion:

--- DSA/recursion/sumofarr.cpp
+++ DSA/recursion/sumofarr.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int smm(int i,vector<int>&arr,int sm){
     if(i==arr.size()-1){
-        return sm;
+        return sm+arr[i];
     }
     sm += arr[i];
-    smm(i+1,arr,sm);
+    return smm(i+1,arr,sm);
 
 }
 
 int main(){
-    vector<int>arr={1,2,3};
+    vector<int>arr={1,2,3,4,5};
     int sm=0;
     cout << smm(0,arr,sm);
     return 1;
