62ffd74f2a52548fb294cfd6c4cccb6c833cd79c
jitesh <jitesh.kumar05official@gmail.com>
2025-05-12 13:18:39 +0530

    recursion

--- DSA/recursion/binsrch.cpp
+++ DSA/recursion/binsrch.cpp
@@ -3,23 +3,25 @@ using namespace std;
 
 int binsearch(int l,int h,int t,vector<int>&nums){
     int mid = l+(h-l)/2;
-    if(l >= h){
+    if(l > h){
         return -1;
     }
     if(nums[mid] < t){
-        binsearch(mid+1,h,t,nums);
+       return  binsearch(mid+1,h,t,nums);
+        
     }else if(nums[mid] > t){
-        binsearch(l,mid-1,t,nums);
+        return binsearch(l,mid-1,t,nums);
     }else{
         // cout << mid;
         return mid;
     }
-    
+    // cout << "Reached" << l << " " << h << " " << mid;
+    return mid;
 }
 
 int main(){
     vector<int>nums = {0,2,4,6,8};
-    int target = 4;
+    int target = 5;
     int i = binsearch(0,4,target,nums);
     cout << i;
     return 0;
