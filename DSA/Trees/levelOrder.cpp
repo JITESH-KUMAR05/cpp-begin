5e4f85c8313617a33784ec8e8f097f31e8f23355
jitesh <jitesh.kumar05official@gmail.com>
2025-06-18 12:10:18 +0530

    did level order tree traversal

---
+++ DSA/Trees/levelOrder.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(int data){
+        val = data;
+        left=nullptr;
+        right=nullptr;
+    }
+};
+/*
+for level order we need one data structure to store the result and one queue for maintaining 
+the current level by which we can check all the childer of the current level 
+if children exist then we will add the children into the level vector and then
+add the level vector to the result vector after checking all the childrent on the current level 
+also one more note that initially we should add the root to the queue inorder algo to work properly
+*/
+
+void levelOrder(TreeNode *root){
+    vector<vector<int>>ans;
+    if(root==nullptr) return;
+    queue<TreeNode*>q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        vector<int>level;
+        for(int i=0;i<size;i++){
+            TreeNode * node = q.front();
+            q.pop();
+            if(node->left != nullptr){
+                q.push(node->left);
+            }
+            if(node->right!=nullptr){
+                q.push(node->right);
+            }
+            cout << node->val << " ";
+            level.push_back(node->val);
+        }
+        cout << endl;
+        ans.push_back(level);
+    }
+
+}
+
+int main(){
+    TreeNode * root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    levelOrder(root);
+    return 0;
+
+}
