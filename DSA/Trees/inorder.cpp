ea078871988edfa5de2270a82c86076cb93fb4ed
jitesh <jitesh.kumar05official@gmail.com>
2025-06-18 11:47:32 +0530

    did inorder tree traversal

--- DSA/Trees/inorder.cpp
+++ DSA/Trees/inorder.cpp
@@ -15,11 +15,20 @@ struct TreeNode{
 void inorder(TreeNode * root){
     if(root==nullptr) return;
     inorder(root->left);
-    cout<<root->val;
+    cout<<root->val << " ";
     inorder(root->right);
 }
 
 int main(){
     TreeNode * root = new TreeNode(1);
-    
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    inorder(root);
+    return 0;
+
 }
