e7f2bf438f5fed76d6a2b95f0c7e106058de1703
jitesh <jitesh.kumar05official@gmail.com>
2025-06-18 11:49:38 +0530

    did preorder tree traversal

---
+++ DSA/Trees/preorder.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(int data){
+        val = data;
+        left=nullptr;
+        right=nullptr;
+    }
+};
+
+void preorder(TreeNode * root){
+    if(root==nullptr) return;
+    cout<<root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main(){
+    TreeNode * root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    preorder(root);
+    return 0;
+
+}
