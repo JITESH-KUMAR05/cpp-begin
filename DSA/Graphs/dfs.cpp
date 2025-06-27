5f33daeea6fe45fd831148343869c5bf21c3574e
jitesh <jitesh.kumar05official@gmail.com>
2025-06-27 17:35:35 +0530

    starting graphs

---
+++ DSA/Graphs/dfs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    void DFS(unordered_map<int, vector<int>>&adj,int u,vector<bool>&visited,vector<int>&result){
+        if(visited[u]==true) return;
+        visited[u] = true;
+        result.push_back(u);
+        for(int &v : adj[u]){
+            if(!visited[v]){
+                DFS(adj,v,visited,result);
+            }
+        }
+    }
+    vector<int> dfs(vector<vector<int>>& mp) {
+        unordered_map<int, vector<int>>adj;
+        
+        for(int u=0;u<mp.size();u++){
+            for(auto v = mp[u].begin();v!=mp[u].end();v++){
+                adj[u].push_back(*v);
+            }
+        }
+        
+        vector<int> result;
+        vector<bool>visited(mp.size(),false);
+        
+        DFS(adj,0,visited,result);
+        return result;
+        
+    }
+};
