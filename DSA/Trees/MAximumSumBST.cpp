67af088aca4e4b2bbb349b24c84eb7c7758c9e8d
Jitesh <jitesh.kumar05official@gmail.com>
2025-08-15 22:01:48 +0530

    changes

--- DSA/Trees/MAximumSumBST.cpp
+++ DSA/Trees/MAximumSumBST.cpp
@@ -55,21 +55,25 @@ struct TreeNode
 class Solution
 {
 public:
-    vector<long long> checkBST(TreeNode *node, long long ans)
+    vector<long long> checkBST(TreeNode *node, long long &ans)
     {
+        if(!node){
+            return {true,LLONG_MAX,LLONG_MIN,0};
+        }
         if (node->left == nullptr && node->right == nullptr)
         {
+            ans = max(ans,(long long) node->val);
             return {true, node->val, node->val, node->val};
         }
 
         if (node->left == nullptr)
         {
             vector<long long> rt = checkBST(node->right, ans);
-            if (node->val <= rt[1])
+            if (rt[0] && node->val < rt[1])
             {
                 long long sum = node->val + rt[3];
                 ans = max(ans, sum);
-                return {true, node->val, rt[2], sum};
+                return {true, min((long long)node->val,rt[1]), max((long long)node->val,rt[2]), sum};
             }
             else
             {
@@ -78,10 +82,10 @@ public:
         }
         if(node->right==nullptr){
             vector<long long>lt=checkBST(node->left,ans);
-            if(node->val >= lt[2]){
+            if(lt[0] && node->val > lt[2]){
                 long long sum = node->val+lt[3];
                 ans=max(ans,sum);
-                return {true,lt[1],node->val,sum};
+                return {true,min((long long)node->val,lt[1]),max((long long)node->val,lt[2]),sum};
             }else{
                 return {false,-1,-1,-1};
             }
@@ -93,21 +97,21 @@ public:
             if(lt[0]==false || rt[0]==false){
                 return {false,-1,-1,-1};
             }
-            if(node->val >= lt[2] && node->val <= rt[1]){
+            if(node->val > lt[2] && node->val < rt[1]){
                 long long sum = lt[3] + rt[3] + node->val;
                 ans = max(ans,sum);
-                return {true,lt[1],rt[2],sum};
+                return {true,min((long long)node->val,lt[1]),max((long long)node->val,rt[2]),sum};
             }
             else{
                 return {false,-1,-1,-1};
             }
         }
-        return {false,-1,-1,-1};
+        return {false,0,0,0};
     }
     int maxSumBST(TreeNode *root)
     {
-        long long min = -1e9;
-        checkBST(root,min);
-        return min;
+        long long maxsum = 0;
+        checkBST(root,maxsum);
+        return maxsum;
     }
 };
