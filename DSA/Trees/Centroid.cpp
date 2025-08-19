f9e16c11b6c7c0af4d13c94961eb9f7c41418175
jitesh <jitesh.kumar05official@gmail.com>
2025-08-19 15:46:14 +0530

    centroid problem

--- DSA/Trees/Centroid.cpp
+++ DSA/Trees/Centroid.cpp
@@ -6,8 +6,8 @@
 // Print one integer: a centroid node. If there are several possibilities, you can choose any of them.
 // Constraints
 
-// 1 \le n \le 2 \cdot 10^5
-// 1 \le a,b \le n
+// 1 <=n <= 2 * 10^5
+// 1 <= a,b <= n
 
 // Example
 // Input:
@@ -19,3 +19,47 @@
 
 // Output:
 // 3
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void findSize(int u,int p,vector<vector<int>>&adj,vector<int>&subtreeSize){
+    subtreeSize[u]=1;
+    for(int v:adj[u]){
+        if(v==p) continue;
+        findSize(v,u,adj,subtreeSize);
+        subtreeSize[u] += subtreeSize[v];
+    }
+}
+
+int findCentroid(int u,int p,vector<vector<int>>&adj,vector<int>&subtreeSize,int n){
+    for(int v:adj[u]){
+        if(v==p)continue;
+        if(subtreeSize[v]> n/2){
+            return findCentroid(v,u,adj,subtreeSize,n);
+        }
+    }
+
+    return u;
+}
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>>adj(n+1);
+    vector<int>subtreeSize(n+1);
+    for(int i=0;i<n-1;i++){
+        int u,v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    findSize(1,0,adj,subtreeSize);
+
+    int centroid = findCentroid(1,0,adj,subtreeSize,n);
+
+    cout << centroid;
+    
+    return 0;
+
+}
