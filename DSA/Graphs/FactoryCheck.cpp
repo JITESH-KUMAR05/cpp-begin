629f2f54e9808f2e5f981e8490e75d0f7f929fd0
jitesh <jitesh.kumar05official@gmail.com>
2025-08-25 15:34:40 +0530

    DSU problem

---
+++ DSA/Graphs/FactoryCheck.cpp
@@ -0,0 +1,33 @@
+// The Graph Factory Check
+
+// Code Monk, eager after learning about Graphs, visited a Graph-making factory where graphs were meticulously crafted. Today, one worker was absent due to illness. Monk decided to fill in and undertake her role.
+
+// The task at hand was to identify whether an incoming graph constituted a tree or not. He was provided with the number of vertices (N) and the degree of each vertex.
+
+// Can you help Monk determine if the graph qualifies as a tree?
+
+// Input Format:
+
+// The first line contains an integer N, denoting the number of vertices.
+// The second line contains N space-separated integers, representing the degrees of the N vertices.
+// Output Format:
+
+// Print "Yes" (without quotes) if the graph is a tree.
+// Print "No" (without quotes) if the graph does not meet the criteria of a tree.
+// Constraints:
+
+// 1 ≤ N ≤ 100
+// 1 ≤ Degreei ≤ 1000
+// Note: A tree is an undirected graph in which any two vertices are connected by exactly one path and contains no cycles.
+
+// Sample Input:
+
+
+// 3
+// 1 2 1
+// Sample Output:
+
+
+// Yes
+
+#include<bits/stdc++.h>
