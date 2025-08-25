48d7641d5d15b4f8c498d4682eafd4441fd1058d
jitesh <jitesh.kumar05official@gmail.com>
2025-08-25 15:53:43 +0530

    DSU problem

--- DSA/Graphs/FactoryCheck.cpp
+++ DSA/Graphs/FactoryCheck.cpp
@@ -31,3 +31,25 @@
 // Yes
 
 #include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int>deg(n);
+
+    for(int i=0;i<n;i++){
+        cin >> deg[i];
+    }
+    int s = 0;
+    for(int val:deg){
+        s += val;
+    }
+    if(s==(2*(n-1))){
+        cout << "Yes";
+    }
+    else{
+        cout << "No";
+    }
+    return 0;
+}
