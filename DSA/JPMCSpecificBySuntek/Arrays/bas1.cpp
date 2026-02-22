5bcb4be34281b728e22b82b48f0e6f1c3b97196a
jitesh <jitesh.kumar05official@gmail.com>
2026-02-22 07:09:07 +0530

    some change

---
+++ DSA/JPMCSpecificBySuntek/Arrays/bas1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    vector<int> arr = {6,5,9,8,9};
+    // find
+    /*
+    1. maximum of the array
+    2. second maxumum
+    3. second max which is not equal to max
+    */
+    if(arr.size() < 2){
+        if(arr.size()==0) {
+            cout << "Array is empty" <<endl;
+            return 0;
+        }
+        else if(arr.size()==1){
+            cout << 
+        }
+    }
+    // maximum
+    
+    return 0;
+}
